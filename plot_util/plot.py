e8812323ba84588a492803b91c79fb666e207b75
Matteo Fordiani <matteofordiani@gmail.com>
2023-02-23 16:17:04 +0100

    Adjust plotting

--- plot_util/plot.py
+++ plot_util/plot.py
@@ -1,5 +1,7 @@
 import os
 import json
+import datetime
+from datetime import datetime, timedelta
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -17,21 +19,31 @@ BRANCH = os.getenv('GITHUB_BRANCH')
 # this sets up the Matplotlib interactive windows, use in ipynb
 # %matplotlib widget
 
-x = json.load(open('data/test-durations.json', 'rb'))
+data = json.load(open('data/test-durations.json', 'rb'))
 
-plt.plot(range(len(x)), list(map(lambda v: v/60, x)), marker='o')
-plt.xlabel('Test Run #')
+plt.plot(range(len(data)), list(map(lambda v: v/60, data)), marker='o')
+plt.xlabel('Date')
+#plt.xlabel('Test Run #')
 plt.ylabel('Duration')
 plt.title(f'{WORKFLOW} ({STATUS} runs on {BRANCH})')
 plt.grid(True)
 
 # Add a vertical line on the events you want to highlight
-plt.axvline(x = 130, color = 'r', linestyle='dashed', label = 'Significant event')
+plt.axvline(x = 132, color = 'r', linestyle='dashed', label = 'Significant event')
+plt.axvline(x = 155, color = 'r', linestyle='dashed', label = 'Significant event')
+
+# Adjust timescale
+last_run_date = datetime(2023, 2, 23)
 
 # Adjust ticks to readable format
 ax = plt.gca()
+
+ax.xaxis.set_major_locator(ticker.MultipleLocator(14))
 ax.yaxis.set_major_locator(ticker.MultipleLocator(30))
-tick_formatter = lambda x, pos: format_timespan(x*60).replace(" minutes", "m").replace(" hours", "h").replace(" hour", "h").replace("and ", "")
-ax.yaxis.set_major_formatter(plt.FuncFormatter(tick_formatter))
+
+xtick_formatter = lambda x, pos: (last_run_date - timedelta(days=(len(data) - x - 1))).strftime('%m/%d/%y')
+ytick_formatter = lambda x, pos: format_timespan(x*60).replace(" minutes", "m").replace(" hours", "h").replace(" hour", "h").replace("and ", "")
+ax.xaxis.set_major_formatter(plt.FuncFormatter(xtick_formatter))
+ax.yaxis.set_major_formatter(plt.FuncFormatter(ytick_formatter))
 
 plt.show()
